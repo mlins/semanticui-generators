a4f6ad4bef49027efa8d5c0f0b5c2ee5d21160cb
Matt Lins <mattlins@gmail.com>
2016-03-29 19:17:28 -0500

    More turbolink power

--- lib/generators/semanticui/install/install_generator.rb
+++ lib/generators/semanticui/install/install_generator.rb
@@ -7,11 +7,7 @@ module Semanticui
       source_root ::File.expand_path('../templates', __FILE__)
 
       def copy_lib
-        directory "lib/templates/erb"
-      end
-
-      def copy_form_builder
-        copy_file "form_builders/form_builder/_form.html.erb", "lib/templates/erb/scaffold/_form.html.erb"
+        directory "lib"
       end
 
       def create_layout
